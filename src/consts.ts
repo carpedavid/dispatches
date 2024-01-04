5b4d3428b532755aa806c62af35607408042529c
David Garrett <carpedavid@gmail.com>
2024-01-03 22:42:23 -0500

    First post

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Dispatches From the Far Realm';
-export const SITE_DESCRIPTION = 'Blog and newsletter of Amalara Game Studio';
+export const SITE_DESCRIPTION = 'The blog and newsletter of Amalara Game Studio. Games and game-adjacent design, review, and discussion.';
